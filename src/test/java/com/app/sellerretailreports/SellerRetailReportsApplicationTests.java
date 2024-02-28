9ef08de885c18dfe616ad92dc059f6bc4b22bc69
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-02-28 11:44:50 +0200

    initial commit

---
+++ src/test/java/com/app/sellerretailreports/SellerRetailReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sellerretailreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerRetailReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
